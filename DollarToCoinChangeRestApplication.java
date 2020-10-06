d283ca62d0e5fa4e665be20e89e92c95e9c35b7c
sandipsa <72237042+sandipsa@users.noreply.github.com>
2020-10-05 21:34:02 -0400

    Update DollarToCoinChangeRestApplication.java

--- DollarToCoinChangeRestApplication.java
+++ DollarToCoinChangeRestApplication.java
@@ -1,6 +1,7 @@
 package com.adp.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DollarToCoinChangeRestApplication{
