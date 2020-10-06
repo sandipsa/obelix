ad63931ac0460298b3dfb8a9bb215b51d0457a56
sandipsa <72237042+sandipsa@users.noreply.github.com>
2020-10-05 22:32:43 -0400

    Update CoinVendingServiceTest.java

--- CoinVendingServiceTest.java
+++ CoinVendingServiceTest.java
@@ -1,6 +1,7 @@
 package com.adp.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.Scanner;
@@ -42,4 +43,16 @@ public class CoinVendingServiceTest{
 		        msg = coinMachine.dispenseCoinsForBills(fileChunks.next());
 			assertEquals("Please enter a numeric dollar amount. The value entered : ws", msg);
  	}
+	
+	@Test
+	public void testDispenseMaxCoins() throws Exception{
+		String msg = coinMachine.dispenseMaxCoinsForDollar("12");
+		assertEquals(", Dimes = 60, Nickels = 100", msg);
+	}
+	
+	@Test
+	public void testAdjustCoinQty() throws Exception{
+		boolean isAdjusted =  coinMachine.adjustQuantity(20, 10, 12, 15);
+		assertTrue(isAdjusted);
+	}
 }
